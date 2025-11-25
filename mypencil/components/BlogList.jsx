cb520dc2b932053d3372bbaa58b4b21aa447d591
santiago-h2o <maxcuello123@gmail.com>
2025-11-24 23:07:42 -0500

    Se crea filtros por categorias

--- mypencil/components/BlogList.jsx
+++ mypencil/components/BlogList.jsx
@@ -1,21 +1,24 @@
 import { blog_data } from '@/assets/assets'
-import React from 'react'
+import React, { useState } from 'react'
 import BlogItem from './BlogItem'
 
 const BlogList = () => {
+
+const [menu, setMenu] = useState("All")
+
     return (
         <div>
             <div className='flex justify-center gap-6 my-10'>
-                <button className='bg-black text-white py-1 px-4 rounded-sm'>Todos</button>
-                <button>Tecnologia</button>
-                <button>Start-Up</button>
-                <button>Buenos Habitos</button>
+                <button onClick={()=>setMenu('All')} className={menu === "All" ? 'bg-black text-white py-1 px-4 rounded-sm':""}>Todos</button>
+                <button onClick={()=>setMenu('Technology')} className={menu === "Technology" ? 'bg-black text-white py-1 px-4 rounded-sm':""}>Tecnologia</button>
+                <button onClick={()=>setMenu('Startup')} className={menu === "Startup" ? 'bg-black text-white py-1 px-4 rounded-sm':""}>Start-Up</button>
+                <button onClick={()=>setMenu('Lifestyle')} className={menu === "Lifestyle" ? 'bg-black text-white py-1 px-4 rounded-sm':""}>Buenos Habitos</button>
             </div>
-<div className='flex flex-wrap justify-around gap-1 gap-y-10 mb-16 xl:mx-24'>
-{blog_data.map((item, index)=>{
-    return <BlogItem key={index} image={item.image} title={item.title} description={item.description} category={item.category}/>
-})}
-</div>
+            <div className='flex flex-wrap justify-around gap-1 gap-y-10 mb-16 xl:mx-24'>
+                {blog_data.filter((item)=> menu === "All" ? true : item.category === menu ).map((item, index) => {
+                    return <BlogItem key={index} image={item.image} title={item.title} description={item.description} category={item.category} />
+                })}
+            </div>
         </div>
     )
 }
