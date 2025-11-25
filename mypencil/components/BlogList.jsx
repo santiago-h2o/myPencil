65be0745128495da7c3d689f8c8ba136d5bde28c
santiago-h2o <maxcuello123@gmail.com>
2025-11-24 22:59:27 -0500

    Se termina de crear el componente de BlogList para mostrar todos los blogs

--- mypencil/components/BlogList.jsx
+++ mypencil/components/BlogList.jsx
@@ -1,11 +1,23 @@
+import { blog_data } from '@/assets/assets'
 import React from 'react'
+import BlogItem from './BlogItem'
 
 const BlogList = () => {
-  return (
-    <div>
-        
-    </div>
-  )
+    return (
+        <div>
+            <div className='flex justify-center gap-6 my-10'>
+                <button className='bg-black text-white py-1 px-4 rounded-sm'>Todos</button>
+                <button>Tecnologia</button>
+                <button>Start-Up</button>
+                <button>Buenos Habitos</button>
+            </div>
+<div className='flex flex-wrap justify-around gap-1 gap-y-10 mb-16 xl:mx-24'>
+{blog_data.map((item, index)=>{
+    return <BlogItem key={index} image={item.image} title={item.title} description={item.description} category={item.category}/>
+})}
+</div>
+        </div>
+    )
 }
 
 export default BlogList
