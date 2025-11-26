f50228639e00744e9471b0ebfe32f12dab86ba4c
santiago-h2o <maxcuello123@gmail.com>
2025-11-26 14:58:15 -0500

    Se realiza la peticion a la api para guardar el blog

--- mypencil/app/admin/layout.jsx
+++ mypencil/app/admin/layout.jsx
@@ -1,11 +1,14 @@
 import { assets } from "@/assets/assets";
 import SideBar from "@/components/admin/SideBar";
 import Image from "next/image";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function Layout({ children }) {
     return (
         <>
             <div className="flex">
+                <ToastContainer theme="dark"/>
                 <SideBar />
                 <div className="flex flex-col w-full">
                     <div className="flex items-center justify-between w-full py-3 max-h-[60px] px-12 border-b border-black">
--- mypencil/components/admin/AddProduct.jsx
+++ mypencil/components/admin/AddProduct.jsx
@@ -1,7 +1,9 @@
 'use client'
 import { assets } from '@/assets/assets'
+import axios from 'axios'
 import Image from 'next/image'
 import React, { useState } from 'react'
+import { toast } from 'react-toastify'
 
 const AddProduct = () => {
 
@@ -21,9 +23,35 @@ const AddProduct = () => {
         console.log(data)
     }
 
+    const onSubmitHandler = async (e) => {
+        e.preventDefault();
+        const formData = new FormData();
+        formData.append('image', image);
+        formData.append('title', data.title);
+        formData.append('description', data.description);
+        formData.append('category', data.category);
+        formData.append('author', data.author);
+        formData.append('authorImage', data.authorImage);
+
+        const response = await axios.post('/api/blog', formData);
+        if (response.data.success) {
+            toast.success(response.data.msg);
+            setImage(false);
+            setData({
+                title: '',
+                description: '',
+                category: 'Startup',
+                author: 'Alex Bennet',
+                authorImage: "/author_img.png"
+            })
+        } else {
+            toast.error("Error");
+        }
+    }
+
     return (
         <>
-            <form action="" className='pt-5 px-5 sm:pt-12 sm:pl-16'>
+            <form onSubmit={onSubmitHandler} action="" className='pt-5 px-5 sm:pt-12 sm:pl-16'>
                 <p className='text-xl'>Subir tnumbnail</p>
                 <label htmlFor="image" className='cursor-pointer'>
                     <Image src={!image ? assets.upload_area : URL.createObjectURL(image)} width={140} height={70} alt='' className='mt-4' />
@@ -32,7 +60,7 @@ const AddProduct = () => {
                 <p className='text-xl mt-4'>Titulo</p>
                 <input onChange={onChangeHandler} value={data.title} name='title' type="text" placeholder='escribe aqui...' className='w-full sm:w-[500px] mt-4 px-4 py-3 border outline-none' required />
                 <p className='text-xl mt-4'>Descripcion</p>
-                <textarea onChange={onChangeHandler} value={data.description} name='description'  type="text" placeholder='escribe aqui el contenido...' rows={6} className='w-full sm:w-[500px] mt-4 px-4 py-3 border outline-none' required />
+                <textarea onChange={onChangeHandler} value={data.description} name='description' type="text" placeholder='escribe aqui el contenido...' rows={6} className='w-full sm:w-[500px] mt-4 px-4 py-3 border outline-none' required />
                 <p className='text-xl mt-4'>Categoria</p>
                 <select name="category" onChange={onChangeHandler} value={data.description} className='w-40 mt-4 px-4 py-3 border text-gray-500 outline-none'>
                     <option value="Startup">Start-Up</option>
